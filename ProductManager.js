705c1128234ce54848a6358d95e6e17e431649e4
jonathan romero fucina <jonathan07_1@hotmail.com>
2023-12-02 22:10:38 -0300

    se trabajo en carpetas js

---
+++ ProductManager.js
@@ -0,0 +1,15 @@
+class ProductManager {
+  constructor() {
+    this.products = [];
+  }
+  getProducts() {
+    return this.products;
+  }
+  addProduct(products){
+    if( products.tittle ||
+        products.description ||
+         
+
+    )
+  }
+}
