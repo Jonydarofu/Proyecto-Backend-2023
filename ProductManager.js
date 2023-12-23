482dcd6b2896d59eca9a32f727a66a5aaf218967
jonathan romero fucina <jonathan07_1@hotmail.com>
2023-12-23 13:19:26 -0300

    se realizo la correccion de ECMAScript

--- ProductManager.js
+++ ProductManager.js
@@ -14,18 +14,17 @@ class ProductManager {
         !products.stock)
 
     { return 'Completar Campos'}
-const result = this.products.find( prod === products.code )
-  if (result)
+const result = this.products.find( (prod) => prod === products.code )
+  if (products)
   return "El producto ya existe"
 }
     if (this.products.length === 0){
     products.id = 1
-    this.products.push ( products )
-} 
-    else{
+    this.products.push ( products )}
+else{
         products.id = this.products.length + 1
     this.products.push ( products )
-    
+   
     return ' producto agregado '
 }
 getProductsByld (pid){
@@ -37,4 +36,4 @@ getProductsByld (pid){
 }
 const products = new ProductManager()
 console.log(products.getProducts())
-console.log(products.addProduct({tittle: 'producto uno', description:'bicicleta Venzo', price:'500000', imagen: 'imagen', stock: '20', code: 'abc123'}))
+console.log(products.addProduct({title: 'producto uno', description:'bicicleta Venzo', price:'500000', imagen: 'imagen', stock: '20', code: 'abc123'}))
