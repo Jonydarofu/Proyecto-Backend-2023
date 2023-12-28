ede1ce73568addde9e153075a8b7236f4e162c6a
jonathan romero fucina <jonathan07_1@hotmail.com>
2023-12-27 21:54:18 -0300

    se trabajo en la primer entrega

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require ('express')
+const userRouter = require('./src/routes/user.router')
+const cartsRouter = require ('./src/routes/carts.router')
+
+ const app = express()
+
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+
+  app.use()
+  app.use()
+  app.use()
+
+  app.listen(,()=>{
+    console.log('Puerto')
+  })
