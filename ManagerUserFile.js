4d020f270c05f063af86145ddcd1402b00bcba4b
jonathan romero fucina <jonathan07_1@hotmail.com>
2024-01-03 00:37:35 -0300

    se trabajo en la primer entrega

--- ManagerUserFile.js
+++ ManagerUserFile.js
@@ -6,11 +6,29 @@ class UserManagerFile {
   constructor() {
     this.path = "./mockDB/Users.json";
   }
-  async createUser() {
+  async readFileUsers() {
     try {
-      const user = await fs.promises("");
+      const userData = await fs.promises.readFile(this.path,'utf-8');
+        const usersJs = await JSON.parse (userData)
+        return usersJs
     } catch (error) {
-      console.log(error);
+      return [];
+    }
+  }
+  async createUser (newUser){
+    try{
+        let usersList = await this.readFileUsers()
+        console.log (newUser)
+        console.log (usersList)
+        newUser.salt = crypto.randomBytes (128).toString ('base64')
+        newUser.newPass = crypto.createHmac('sha256',newUser.salt).update(newUser.password).digest('hex')
+        console.log(newUser)
+
+        usersList.push(newUser)
+        await fs.promises.writeFile(this.path, JSON.stringify(usersList. null, 2))
+
+    } catch (error){
+        console.log(error)
     }
   }
   async validateUser() {
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require ('express')
-const userRouter = require('./src/routes/user.router')
-const cartsRouter = require ('./src/routes/carts.router')
+const express = require("express");
+const userRouter = require("./src/routes/user.router");
+const cartsRouter = require("./src/routes/carts.router");
 
- const app = express()
+const app = express();
 
-  app.use(express.json())
-  app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  app.use()
-  app.use()
-  app.use()
+app.use();
+app.use();
+app.use();
 
-  app.listen(,()=>{
-    console.log('Puerto')
-  })
+app.listen(() => {
+  console.log("Puerto");
+});
