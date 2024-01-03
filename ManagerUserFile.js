353d8bbd5b4050f3e709372124602afb4f4daef6
jonathan romero fucina <jonathan07_1@hotmail.com>
2024-01-03 00:58:05 -0300

    se trabajo en la primer entrega

--- ManagerUserFile.js
+++ ManagerUserFile.js
@@ -8,33 +8,54 @@ class UserManagerFile {
   }
   async readFileUsers() {
     try {
-      const userData = await fs.promises.readFile(this.path,'utf-8');
-        const usersJs = await JSON.parse (userData)
-        return usersJs
+      const userData = await fs.promises.readFile(this.path, "utf-8");
+      const usersJs = await JSON.parse(userData);
+      return usersJs;
     } catch (error) {
       return [];
     }
   }
-  async createUser (newUser){
-    try{
-        let usersList = await this.readFileUsers()
-        console.log (newUser)
-        console.log (usersList)
-        newUser.salt = crypto.randomBytes (128).toString ('base64')
-        newUser.newPass = crypto.createHmac('sha256',newUser.salt).update(newUser.password).digest('hex')
-        console.log(newUser)
+  async createUser(newUser) {
+    try {
+      let usersList = await this.readFileUsers();
+      console.log(newUser);
+      console.log(usersList);
+      newUser.salt = crypto.randomBytes(128).toString("base64");
+      newUser.newPass = crypto
+        .createHmac("sha256", newUser.salt)
+        .update(newUser.password)
+        .digest("hex");
+      console.log(newUser);
 
-        usersList.push(newUser)
-        await fs.promises.writeFile(this.path, JSON.stringify(usersList. null, 2))
-
-    } catch (error){
-        console.log(error)
+      usersList.push(newUser);
+      await fs.promises.writeFile(this.path, JSON.stringify(usersList.null, 2));
+    } catch (error) {
+      console.log(error);
     }
   }
   async validateUser() {
     try {
+      const Users = await this.readFileUsers();
+      const user = user.find((prod) => prod.nombre === nombre);
+      const hashPass = crypto
+        .createHmac("sha256", user.salt)
+        .update(password)
+        .digest("hex");
+      if ((hashPass === user, newPass)) {
+        return "true";
+      }
+      return "false";
     } catch (error) {
       console.log(error);
     }
   }
 }
+const users = new UserManagerFile();
+console.log(
+  users.createUser({
+    nombre: "Jonathan",
+    apellido: "Romero Fucina",
+    username: "jonydarofu",
+    password: "123456",
+  })
+);
