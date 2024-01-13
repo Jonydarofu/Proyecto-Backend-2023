ad1ff2d7e69aa6a26f25da8cdf7033762e85b967
jonathan romero fucina <jonathan07_1@hotmail.com>
2024-01-12 22:57:07 -0300

    se arreglo servidor express

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const userRouter = require("./src/routes/user.router");
 const cartsRouter = require("./src/routes/carts.router");
-
+const ProductManager = require("./ProductManager")
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use();
-app.use();
-app.use();
+let products = [
+  { id: 1, nombre: "Venzo", linea: "Thorn" },
+  { id: 2, nombre: "Venzo", linea: "Loki" },
+  { id: 3, nombre: "Venzo", linea: "Skyline" },
+];
+app.get('/api/product/:uid', (req, res)=>{
+  const {uid} = req.params
+  const product = product.find(product => product.id === Number(uid))
+  res.send (product)
+});
+
+app.get('/users', (req,res)=> {
+  res.send (products)
+});
+
 
 app.listen(() => {
   console.log("Puerto");
