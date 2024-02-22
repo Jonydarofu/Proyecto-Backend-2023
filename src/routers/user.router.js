97419e6ed6e747c262b7ea91960acbf176949aa6
jonathan romero fucina <jonathan07_1@hotmail.com>
2024-02-22 04:59:33 -0300

    coregimos proyecto moongose

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,10 +1,26 @@
-const express = require("express");
+const { Router } = require("express");
+const { userModel } = require("../models/user.model");
 
 const router = express.Router();
+userModel
 
 router
-  .get("/", () => {})
-  .post("/:cid", () => {})
-  .post("/", () => {});
+  .get("/", (req, res) => {
+    try{
+      const user = await userModel.find({})
+    res.send(users);
+  }
+catch (error){
+  console.log(error)}
+})
+  .get("/:uid", (req, res) => {
+    const { uid } = req.params;
+    res.send("user");
+  })
+  .post("/", (req, res) => {
+    const {nombre, apellido} = req.body
+    if (!nombre || !apellido ) {
+      return res.status(400).send({ error: "Faltan datos"})
+  }});
 
 module.exports = router;
