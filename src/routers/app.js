77da5ab6e6b3503cd846a527b5f3d7d92a2d3b53
jonathan romero fucina <jonathan07_1@hotmail.com>
2024-03-04 23:58:32 -0300

    coregimos proyecto moongose

--- src/routers/app.js
+++ src/routers/app.js
@@ -9,11 +9,16 @@ app.use(express.json())
 app.use(express.urlencoded({extends:true}))
 app.use(logger('dev'))
 
+app.engine ("handlebars", handlebars.engine())
+app.set('views, __dirname +  "/view"')
+app.set("view engine", "handlebars")
+
+app.use(appRouter)
 
 app.get("/", (request, response) => {
   response.send("Hola mundo");
 });
 app.listen(PORT, (err) => {
   if (err) console.log(err);
-  console.log();
+  console.log('Servidor escuchando en el puerto ${PORT}');
 });
